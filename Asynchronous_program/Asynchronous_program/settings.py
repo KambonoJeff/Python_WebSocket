c9f3122b78bfffb37442388ee74ab59d20459467
KAMBONO <kambonoj@gmail.com>
2023-05-20 08:33:17 +0300

    Initial commit

--- Asynchronous_program/Asynchronous_program/settings.py
+++ Asynchronous_program/Asynchronous_program/settings.py
@@ -10,6 +10,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
